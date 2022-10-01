1bb4e16361d855085aac079d3f9fc5bba78dc275
mazoonit <smazen98@gmail.com>
2022-10-01 08:19:20 +0200

    validating over event/post endpoint without action validation and metadata validation.

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -91,56 +91,69 @@ router.get('/', async (req: Request, res: Response, next: Function) => {
   }
 });
 
-router.post('/', async (req: Request, res: Response, next: Function) => {
-  //serializer
-  try {
-    let event = serializer(['location', 'occurred_at'], req.body);
-    let { actor_email, actor_name, target_name, group, action, metadata } = req.body;
+router.post(
+  '/',
+  [
+    validatorMiddleware([
+      { type: 'string', key: 'actor_email', required: true },
+      { type: 'string', key: 'actor_name', required: true },
+      { type: 'string', key: 'target_name', required: true },
+      { type: 'string', key: 'group', required: true },
+      { type: 'date', key: 'occurred_at', required: true },
+      { type: 'string', key: 'location', required: true },
+    ]),
+  ],
+  async (req: Request, res: Response, next: Function) => {
+    //serializer
+    try {
+      let event = serializer(['location', 'occurred_at'], req.body);
+      let { actor_email, actor_name, target_name, group, action, metadata } = req.body;
 
-    // get or create new object of actor/group/target
-    //group
-    let group_id = await getObjectIdByAnotherField({
-      searchField: 'name',
-      value: group,
-      tableName: 'group',
-      data: { name: group },
-    });
-    //actor
-    let actor_id = await getObjectIdByAnotherField({
-      searchField: 'email',
-      value: actor_email,
-      tableName: 'actor',
-      data: { name: actor_name, email: actor_email },
-    });
-    //target
-    let target_id = await getObjectIdByAnotherField({
-      searchField: 'name',
-      value: target_name,
-      tableName: 'target',
-      data: { name: target_name },
-    });
-    //Glowing ids to the event object.
-    event.group_id = group_id;
-    event.target_id = target_id;
-    event.actor_id = actor_id;
+      // get or create new object of actor/group/target
+      //group
+      let group_id = await getObjectIdByAnotherField({
+        searchField: 'name',
+        value: group,
+        tableName: 'group',
+        data: { name: group },
+      });
+      //actor
+      let actor_id = await getObjectIdByAnotherField({
+        searchField: 'email',
+        value: actor_email,
+        tableName: 'actor',
+        data: { name: actor_name, email: actor_email },
+      });
+      //target
+      let target_id = await getObjectIdByAnotherField({
+        searchField: 'name',
+        value: target_name,
+        tableName: 'target',
+        data: { name: target_name },
+      });
+      //Glowing ids to the event object.
+      event.group_id = group_id;
+      event.target_id = target_id;
+      event.actor_id = actor_id;
 
-    //create action and glow its Id to the event object.
-    let createdAction = await prisma.action.create({
-      data: { name: action.name },
-    });
-    event.action_id = createdAction.id;
+      //create action and glow its Id to the event object.
+      let createdAction = await prisma.action.create({
+        data: { name: action.name },
+      });
+      event.action_id = createdAction.id;
 
-    //parse metadata into string. and glowing it to the event object.
-    event.meta_data = JSON.stringify(metadata);
+      //parse metadata into string. and glowing it to the event object.
+      event.meta_data = JSON.stringify(metadata);
 
-    let createdEvent = await prisma.event.create({
-      data: { ...event },
-    });
+      let createdEvent = await prisma.event.create({
+        data: { ...event },
+      });
 
-    return res.status(200).send(createdEvent);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+      return res.status(200).send(createdEvent);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  },
+);
 export default router;
